4daa633095e34f1789631642c2405f9f367685b4
Dominic <dcontrer@andrew.cmu.edu>
2020-10-13 21:39:55 -0400

    Input selectors all working, next step work on cleaning code and generating formula, regression outputs and plots. Hoping that someone on Stack can show me how to store selected variables as things change

--- app.R
+++ app.R
@@ -4,13 +4,18 @@ library(shiny)
 
 ## stored data objects, including selector options, and count of predictors
 dat <- mtcars %>% 
-  mutate(qual = sample(c("good", "bad", "horrible"), size = nrow(mtcars), replace = TRUE) %>% as.factor,
-         speed = sample(c("fast", "slow", "buzz"), size = nrow(mtcars), replace = TRUE) %>% as.factor)
+  mutate(qual = sample(c("good", "bad", "horrible"), 
+                       size = nrow(mtcars), 
+                       replace = TRUE) %>% as.factor,
+         speed = sample(c("fast", "slow", "buzz"), 
+                        size = nrow(mtcars), 
+                        replace = TRUE) %>% as.factor)
+
 choice_list <- list(continuous = dat %>% select_if(is.numeric) %>% colnames %>% sort,
                     categorical = dat %>% select_if(is.factor) %>% colnames %>% sort)
 
 ## transformation options
-target_trans <- list("Simple" = c("Reciprocal", "Squared"),
+target_trans <- list("Simple" = c("None", "Reciprocal", "Squared"),
                      "Logarithmic" = c("Natural", "Base 2", "Base 10"),
                      "Root" = c("Cubed", "Square"))
 pred_trans <- target_trans
@@ -33,17 +38,20 @@ ui <- fluidPage(
   fluidRow(
     ## input selector for target variable
     column(width = 2, 
-           selectInput(inputId = "target", 
-                       label = "Target Variable:", 
-                       choices = choice_list[["continuous"]])),
+           selectizeInput(inputId = "target", 
+                          label = "Target Variable:", 
+                          choices = choice_list[["continuous"]],
+                          multiple = TRUE,
+                          selected = NULL,
+                          options = list(placeholder = "Click to select",
+                                         maxItems = 1))),
     ## input selector for target variable transformation, excluding polynomials
     column(width = 2, 
            selectizeInput(inputId = "trans", 
                           label = "Transformation:",
                           choices = target_trans,
-                          multiple = TRUE,
-                          options = list(placeholder = "None",
-                                         maxItems = 1)))),
+                          multiple = FALSE,
+                          selected = "None"))),
   fluidRow(
     ## input selector for number of predictors
     column(width = 2,
@@ -58,7 +66,11 @@ ui <- fluidPage(
                           options = list(placeholder = "None")))),
   HTML("<p style=\"font-size:14.5px\"><b>Continuous Predictor <br> Transformations: </b></p>"),
   ## stored layout for dynamic UI
-  uiOutput("preds_ui")
+  uiOutput("preds_ui"),
+  fluidRow(column(width = 2, actionButton("intTermAdd", "Add interaction term")),
+           column(width = 2, actionButton("intTermRemove", "Remove interaction term"))),
+  div(style = "height:25.5px"),
+  uiOutput("intUI")
 )
 
 server <- function(input, output, session) {
@@ -70,12 +82,11 @@ server <- function(input, output, session) {
                    label = "Select Continuous Predictors", 
                    choices = continuous_preds(),
                    multiple = TRUE,
-                   options = list(placeholder = "None"))
+                   options = list(placeholder = ifelse(length(input$target) == 0, 
+                                                       "Select target variable", 
+                                                       "None")))
   )
   output$preds_ui <- renderUI({
-    observeEvent(input$cont_preds, {
-      
-    })
     row_idx <- length(input$cont_preds) %>% seq_len
     row_idx <- row_idx[row_idx %% 2 == 1]
     row_idx %>% 
@@ -88,9 +99,7 @@ server <- function(input, output, session) {
                    choices = pred_trans,
                    multiple = TRUE,
                    options = list(placeholder = "None",
-                                  maxItems = 1),
-                   selected = isolate(input$cont_preds[.x]))
-                 ),
+                                  maxItems = 1))),
           column(width = 2, 
                  selectizeInput(
                    inputId = paste0("cont_pred_trans", .x + 1), 
@@ -98,9 +107,8 @@ server <- function(input, output, session) {
                    choices = pred_trans,
                    multiple = TRUE,
                    options = list(placeholder = "None",
-                                  maxItems = 1),
-                   selected = isolate(input$cont_preds[.x + 1]))
-                 ))
+                                  maxItems = 1))
+          ))
       } else {
         fluidRow(
           column(width = 2, 
@@ -109,10 +117,33 @@ server <- function(input, output, session) {
                                 choices = pred_trans,
                                 multiple = TRUE,
                                 options = list(placeholder = "None",
-                                               maxItems = 1),
-                                selected = isolate(input$cont_preds[.x + 1]))))
+                                               maxItems = 1))))
       }
-      %||% "")
+      )
+  })
+  output$intUI <- renderUI({
+    intChoices <- c(input$cont_preds, input$cat_preds)
+    intTermCount <- input$intTermAdd + input$intTermRemove * - 1
+    intTermCountSeq <- seq_len(intTermCount)
+    intTermCountSeq %>% 
+      map(~fluidRow(
+        column(width = 2, 
+               selectizeInput(
+                 inputId = paste0("term", .x), 
+                 label = paste("Term", .x),
+                 choices = intChoices,
+                 multiple = TRUE,
+                 options = list(placeholder = "None",
+                                maxItems = 1))),
+        column(width = 2, 
+               selectizeInput(
+                 inputId = paste0("term", .x + 1), 
+                 label = paste("Term", .x + 1),
+                 choices = intChoices,
+                 multiple = TRUE,
+                 options = list(placeholder = "None",
+                                maxItems = 1))
+        )))
   })
 }
 
