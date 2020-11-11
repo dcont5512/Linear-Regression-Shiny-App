98af3a21db43c23043f60ebb2af977919e8bd2a5
Dominic <dcontrer@andrew.cmu.edu>
2020-11-11 09:44:31 -0500

    recode function much better now due to help from rstudio community

--- app.R
+++ app.R
@@ -81,20 +81,21 @@ variable. You must selected at least two categorical variables for the fill opti
 fill is selected, the plot are still colored, although the fill is based on the underlying variable categories.</p>"
 
 ## source packages for data frames
-pkg_list <- c("mlbench", "AppliedPredictiveModeling", "datasets")
+#pkg_list <- c("mlbench", "AppliedPredictiveModeling", "datasets")
 ## obtain names of data objects in each package
-object_list <- pkg_list %>% 
-  map(~list(.x = data(package = .x)$results %>%
-              data.frame %>% .$Item %>% sort %>% as.character)) %>% unlist %>% as.character
+#object_list <- pkg_list %>% 
+#m ap(~list(.x = data(package = .x)$results %>%
+#            data.frame %>% .$Item %>% sort %>% as.character)) %>% unlist %>% as.character
 ## exclude any objects with parenthesis in name
-object_list <- object_list[!grepl("\\(", object_list)]
+# object_list <- object_list[!grepl("\\(", object_list)]
 ## load all remaining objects into environment
-data(list = object_list)
+# data(list = object_list)
 ## determine whether object is a data frame
-data_list <- object_list %>% map(~(is.data.frame(get(.x)))) %>% as.character 
-names(data_list) <- object_list
+# data_list <- object_list %>% map(~(is.data.frame(get(.x)))) %>% as.character 
+# names(data_list) <- object_list
 ## keep only names of data frames
-choice_list <- data_list %>% .[matches("TRUE", vars=.)] %>% names %>% sort
+# choice_list <- data_list %>% .[matches("TRUE", vars=.)] %>% names %>% sort
+choice_list <- c("diamonds", "mtcars")
 
 ## transformation options
 tran_opts <- list("Simple" = c("None",  "Reciprocal", "Squared", "Square Root"),
@@ -195,37 +196,38 @@ ui <-  dashboardPage(
                uiOutput("data_sample_ui")),
       menuItem("Recode Variables",
                uiOutput("data_recode")),
-      fluidRow(
-        column(width = 6,
-               ## target variable select
-               selectizeInput(inputId = "target", label = "Target Variable:", 
-                              choices = "",
-                              multiple = TRUE,
-                              selected = NULL,
-                              options = list(placeholder = "Click to select",
-                                             maxItems = 1))),
-        column(width = 6, 
-               ## target variable transformation (exclude polynomials)
-               selectizeInput(inputId = "tran_target", 
-                              label = "Transformation:",
-                              choices = tran_opts[c("Simple", "Logarithmic")],
-                              multiple = FALSE,
-                              selected = "None"))),
-      fluidRow(
-        column(width = 6,
-               ## continuous variables select
-               selectizeInput(inputId = "preds_cont", 
-                              label = "Continuous Variables:", 
-                              choices = "",
-                              multiple = TRUE,
-                              options = list(placeholder = "None"))),
-        column(width = 6,
-               ## categorical variables select
-               selectizeInput(inputId = "preds_cat",
-                              label = "Categorical Variables:",
-                              choices = "",
-                              multiple = TRUE,
-                              options = list(placeholder = "None")))),
+      menuItem("Model Terms", 
+               fluidRow(
+                 column(width = 6,
+                        ## target variable select
+                        selectizeInput(inputId = "target", label = "Target Variable:", 
+                                       choices = "",
+                                       multiple = TRUE,
+                                       selected = NULL,
+                                       options = list(placeholder = "Click to select",
+                                                      maxItems = 1))),
+                 column(width = 6, 
+                        ## target variable transformation (exclude polynomials)
+                        selectizeInput(inputId = "tran_target", 
+                                       label = "Transformation:",
+                                       choices = tran_opts[c("Simple", "Logarithmic")],
+                                       multiple = FALSE,
+                                       selected = "None"))),
+               fluidRow(
+                 column(width = 6,
+                        ## continuous variables select
+                        selectizeInput(inputId = "preds_cont", 
+                                       label = "Continuous Variables:", 
+                                       choices = "",
+                                       multiple = TRUE,
+                                       options = list(placeholder = "None"))),
+                 column(width = 6,
+                        ## categorical variables select
+                        selectizeInput(inputId = "preds_cat",
+                                       label = "Categorical Variables:",
+                                       choices = "",
+                                       multiple = TRUE,
+                                       options = list(placeholder = "None"))))),
       ## continuous variable transformation ui
       menuItem("Variable Transformations",
                uiOutput(outputId = "preds_tran_ui")),
@@ -357,29 +359,6 @@ server <- function(input, output, session) {
       fileInput(inputId = "data_file", label =  "Upload File:", accept = c(".RData"))
     }
   })
-  ## data environment
-  dat <- reactive({
-    if(input$data_source == "BaseR") {
-      dat <-  get(input$data_file)
-    } else {
-      file_type <- tools::file_ext(input$data_file$name)
-      switch(file_type,
-             RData = load(input$data_file$datapath),
-             validate("Invalid file; Please upload a .RData file")
-      )
-      dat <- sub(".RData$", "", basename(input$data_file$name)) %>% get
-    }
-    if(input$data_complete == "Yes" & input$data_sample == "Yes") {
-      na.omit(dat) %>% 
-        sample_n(input$data_sample_size)
-    } else if (input$data_complete == "Yes" & input$data_sample == "No") {
-      na.omit(dat)
-    } else if (input$data_complete == "No" & input$data_sample == "Yes") {
-      dat %>% sample_n(input$data_sample_size)
-    } else {
-      dat
-    } 
-  })
   ## data environment for sample size
   dat_sample <- reactive({
     if(input$data_source == "BaseR") {
@@ -408,14 +387,40 @@ server <- function(input, output, session) {
                   value = nrow(dat_sample()), width = "100%")
     }
   })
-  ## data recode default options
-  var_recode_default <- reactive({
-    sapply(dat(), function(x) if(is.factor(x)) "factor" else toString(class(x))) %>% 
+  ## data environment for app usage
+  dat <- reactive({
+    if(input$data_source == "BaseR") {
+      dat <-  get(input$data_file)
+    } else {
+      file_type <- tools::file_ext(input$data_file$name)
+      switch(file_type,
+             RData = load(input$data_file$datapath),
+             validate("Invalid file; Please upload a .RData file")
+      )
+      dat <- sub(".RData$", "", basename(input$data_file$name)) %>% get
+    }
+    if(input$data_complete == "Yes" & input$data_sample == "Yes") {
+      na.omit(dat) %>% 
+        sample_n(input$data_sample_size)
+    } else if (input$data_complete == "Yes" & input$data_sample == "No") {
+      na.omit(dat)
+    } else if (input$data_complete == "No" & input$data_sample == "Yes") {
+      dat %>% sample_n(input$data_sample_size)
+    } else {
+      dat
+    } 
+  })
+  ## data recode default options function
+  var_orig_func <- function(x) {
+    sapply(x, function(y) if(is.factor(y)) "factor" else toString(class(y))) %>% 
       data.frame %>% 
       rownames_to_column() %>% 
       set_names(c("var_name", "var_type")) %>% 
-      mutate(var_type = ifelse(var_type %in% c("numeric", "integer"), "continuous", "categorical"))})
-  ## generate recode UI
+      mutate(var_type = ifelse(var_type %in% c("numeric", "integer"), "continuous", "categorical"))
+  }
+  ## data recode default options
+  var_recode_default <- reactive(var_orig_func(dat()))
+  ## generate recode 
   output$data_recode <- renderUI({
     # recode selectors
     var_recode_select <-  function(var_name, var_type) {
@@ -451,11 +456,21 @@ server <- function(input, output, session) {
                                           var_type = recode_left_type)))
       })
   })
-  ## recoded data
+  ## generate reactive data frame based on variable recoding
   dat_recode <- reactive({
-    data.frame(var = var_recode_default() %>% .$var_name,
-               var_recode = var_recode_default() %>% .$var_name %>%
-                 map(~input[[paste0(.x, "_recode")]]) %>% unlist) %>%
+    vn <- var_recode_default() %>% .$var_name
+    vrc <- vn %>%
+      map(~input[[paste0(.x, "_recode")]]) %>% unlist
+    
+    cat("vn length ", length(vn), " :content: ",vn,"\n")
+    cat("vrc length ", length(vrc), " :content: ",vrc,"\n")
+    if (length(vrc)!=length(vn)) {
+      vrc <- var_orig_func(dat()) %>% 
+        .$var_type
+      cat("* ALTERED vrc length ", length(vrc), " :content: ",vrc,"\n")
+    }
+    data.frame(var = vn,
+               var_recode = vrc) %>%
       mutate(var_recode_statement = paste0(var, " %>% ",
                                            ifelse(var_recode == "continuous", "as.numeric", "as.factor"))) %>%
       pmap(~dat() %>%
@@ -465,13 +480,12 @@ server <- function(input, output, session) {
   })
   # data table render
   output$data <- renderDataTable({
-    req(input[[paste0(dat_vars()[["continuous"]][[1]], "_recode")]])
     if(length(input$data_file) != 0) {
-      datatable(dat_recode(), 
-                options = list(pageLength = 5,
-                               lengthMenu = list(c(5, 10, 25, 50), 
-                                                 c("5", "10", "25", "50")),
-                               scrollX = TRUE))
+        datatable(dat_recode(), 
+                  options = list(pageLength = 5,
+                                 lengthMenu = list(c(5, 10, 25, 50), 
+                                                   c("5", "10", "25", "50")),
+                                 scrollX = TRUE))
     }
   })
   ## data help file
@@ -490,10 +504,9 @@ server <- function(input, output, session) {
   })
   ## variable names
   dat_vars <- reactive({
-    req(input$data_file)
-    list(continuous = dat() %>% select_if(is.numeric) %>% colnames %>% sort,
+    list(continuous = dat_recode() %>% select_if(is.numeric) %>% colnames %>% sort,
          categorical = c("is.factor", "is.character") %>% 
-           map(~select_if(dat(), .x) %>% 
+           map(~select_if(dat_recode(), .x) %>% 
                  colnames) %>% unlist %>% sort)
   })
   ## target variable choices
@@ -517,12 +530,12 @@ server <- function(input, output, session) {
   })
   ## correlation matrix ui
   observeEvent(input$corr_matrix_generate, {
-    browser()
     output$corr_matrix_ui <- renderUI(plotOutput(outputId = "corr_matrix", height = 600))
   })
   ## correlation matrix render
   observeEvent(input$corr_matrix_generate, {
     output$corr_matrix <- renderPlot({
+      browser()
       Waiter$new(id = "corr_matrix")$show()
       dat() %>% select_if(is.numeric) %>% ggpairs})
   })
